b50214750e83a0f650df2b1b7266098c3b3756b7
João Luiz <jleaopitta@gmail.com>
2021-03-27 19:49:45 -0300

    adicionado features de subscribe, junto com a conexão com stripe e prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -7,8 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
